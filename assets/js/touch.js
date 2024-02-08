2957452c2d4d1b3ae6a3727f6a740ab28bac3e03
tortank076 <tortank76@hotmail.fr>
2024-02-08 16:24:45 +0100

    peach

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,18 +1,23 @@
 var pacman = document.getElementById("touch");
-var hammertime = new Hammer(document.body, {});
 
+var hammertime = new Hammer(document.body, {});
+var ofLeft = touch.offsetLeft;
+var ofTop = touch.offsetTop;
 hammertime.on('pan pinch', function (event) {
-    
+    console.dir(touch.offsetLeft);
+    pacman.style.left = touch.offsetLeft + "px";
     let distance = event.distance;
     let additionalEvent = event.additionalEvent;
     console.log(additionalEvent, distance);
     // "panright" "panleft" "panup" "pandown"
     switch (additionalEvent) {
         case "panright":
-            pacman.style.transform = "translateX(" + distance + "px)";
+            ofLeft = (distance + ofLeft)/2;
+            pacman.style.left = ofLeft + "px";
             break;
         case "panleft":
-            pacman.style.transform = "translateX(-" + distance + "px)";
+            ofLeft = (ofLeft - distance)/2;
+            pacman.style.left = ofLeft + "px";
             break;
         case "panup":
             pacman.style.transform = "translateY(-" + distance + "px)";
@@ -23,4 +28,4 @@ hammertime.on('pan pinch', function (event) {
         default:
             break;
     }
-});
+});pacman
